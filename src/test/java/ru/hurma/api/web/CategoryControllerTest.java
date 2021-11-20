c3cd374e9f91ef9cdc77e65b8826488f9361d133
Aleksandr Uvarov <aleksandr.o.uvarov@raiffeisen.ru>
2021-11-20 15:05:38 +0300

    Fix tests

--- src/test/java/ru/hurma/api/web/CategoryControllerTest.java
+++ src/test/java/ru/hurma/api/web/CategoryControllerTest.java
@@ -63,20 +63,20 @@ class CategoryControllerTest {
                 .andExpect(jsonPath("$[0].items").isArray())
 
                 .andExpect(jsonPath("$[0].items[0].id", is(apple.getId().intValue())))
-                .andExpect(jsonPath("$[0].items[0].bought", is(apple.isBought())))
+                .andExpect(jsonPath("$[0].items[0].bought", is(apple.getBought())))
                 .andExpect(jsonPath("$[0].items[0].category", is(apple.getCategory().getName())))
                 .andExpect(jsonPath("$[0].items[0].comment", is(apple.getComment())))
-                .andExpect(jsonPath("$[0].items[0].important", is(apple.isImportant())))
+                .andExpect(jsonPath("$[0].items[0].important", is(apple.getImportant())))
                 .andExpect(jsonPath("$[0].items[0].name", is(apple.getName())))
-                .andExpect(jsonPath("$[0].items[0].needed", is(apple.isNeeded())))
+                .andExpect(jsonPath("$[0].items[0].needed", is(apple.getNeeded())))
 
                 .andExpect(jsonPath("$[0].items[1].id", is(lemon.getId().intValue())))
-                .andExpect(jsonPath("$[0].items[1].bought", is(lemon.isBought())))
+                .andExpect(jsonPath("$[0].items[1].bought", is(lemon.getBought())))
                 .andExpect(jsonPath("$[0].items[1].category", is(lemon.getCategory().getName())))
                 .andExpect(jsonPath("$[0].items[1].comment", is(lemon.getComment())))
-                .andExpect(jsonPath("$[0].items[1].important", is(lemon.isImportant())))
+                .andExpect(jsonPath("$[0].items[1].important", is(lemon.getImportant())))
                 .andExpect(jsonPath("$[0].items[1].name", is(lemon.getName())))
-                .andExpect(jsonPath("$[0].items[1].needed", is(lemon.isNeeded())))
+                .andExpect(jsonPath("$[0].items[1].needed", is(lemon.getNeeded())))
 
                 .andExpect(jsonPath("$[1].id", is(vegCategory.getId().intValue())))
                 .andExpect(jsonPath("$[1].name", is(vegCategory.getName())))
@@ -85,12 +85,12 @@ class CategoryControllerTest {
                 .andExpect(jsonPath("$[1].items").isArray())
 
                 .andExpect(jsonPath("$[1].items[0].id", is(tomato.getId().intValue())))
-                .andExpect(jsonPath("$[1].items[0].bought", is(tomato.isBought())))
+                .andExpect(jsonPath("$[1].items[0].bought", is(tomato.getBought())))
                 .andExpect(jsonPath("$[1].items[0].category", is(tomato.getCategory().getName())))
                 .andExpect(jsonPath("$[1].items[0].comment", is(tomato.getComment())))
-                .andExpect(jsonPath("$[1].items[0].important", is(tomato.isImportant())))
+                .andExpect(jsonPath("$[1].items[0].important", is(tomato.getImportant())))
                 .andExpect(jsonPath("$[1].items[0].name", is(tomato.getName())))
-                .andExpect(jsonPath("$[1].items[0].needed", is(tomato.isNeeded())));
+                .andExpect(jsonPath("$[1].items[0].needed", is(tomato.getNeeded())));
     }
 
     @TestConfiguration
