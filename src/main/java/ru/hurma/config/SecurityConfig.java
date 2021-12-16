87647dad0c8147eacf4fc694bf5ebf8d05880b90
Aleksandr Uvarov <sanyauvarov@mail.ru>
2021-12-16 15:46:18 +0300

    Move user credentials to props

--- src/main/java/ru/hurma/config/SecurityConfig.java
+++ src/main/java/ru/hurma/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package ru.hurma.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,13 +11,16 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Value("${hurma.username:User}") private String username;
+    @Value("${hurma.password}") private String password;
+
     @Bean
     @Override
     public UserDetailsService userDetailsService() {
         return new InMemoryUserDetailsManager(
                 User.withDefaultPasswordEncoder()
-                        .username("test")
-                        .password("test")
+                        .username(username)
+                        .password(password)
                         .roles("USER")
                         .build()
         );
